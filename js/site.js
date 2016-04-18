d9b451e9de181961a097a05ee84d8c2fdf2b8e2b
Abhishek.Lal <Abhishek.Lal@hcl.com>
2016-04-18 15:37:30 +0530

    Initial Check In for repository
    Initial Code Checkin

---
+++ js/site.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.diff-email label').click(function () {
+        $(this).hide();
+        $('#diff-email').show();
+    });
+    $('.modal-footer .btn-primary,.close').click(function () {
+        $('#diff-email').val($(this).attr('placeholder'));
+        $('#diff-email').hide();
+        $('.diff-email label').show();
+    });
+});
