8e338f1c3635b1b7c3fa14381394ef3e67b69921
Abhishek.Lal <Abhishek.Lal@hcl.com>
2016-05-06 17:45:47 +0530

    Reports, site.js, css-reset

--- js/site.js
+++ js/site.js
@@ -39,6 +39,35 @@
         $('[data-toggle="tooltip"]').tooltip()
     })
 
+    $("#carousel-one .right").click(function () {
+        if ($(".active h2").html() == 'Funnel') {
+            $('#carousel-two').removeClass('hide');
+        }
+        else {
+            $('#carousel-two').addClass('hide');
+        }
+        if ($(".active h2").html() == 'Program Time to Fill') {
+            $('#carousel-three').removeClass('hide');
+        }
+        else {
+            $('#carousel-three').addClass('hide');
+        }
+    });
+    $("#carousel-one .left").click(function () {
+        if ($(".active h2").html() == 'Req Stage Gate') {
+            $('#carousel-two').removeClass('hide');
+        }
+        else {
+            $('#carousel-two').addClass('hide');
+        }
+        if ($(".active h2").html() == 'Supplier Score Card') {
+            $('#carousel-three').removeClass('hide');
+        }
+        else {
+            $('#carousel-three').addClass('hide');
+        }
+    });
+
     //$('.button-indicator .btn').click(function () {
     //    $(this).toggleClass('btn-default btn-clearbg');
     //});
